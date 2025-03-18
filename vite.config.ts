138c927cedf754367d403469bd2f07de3a6263a9
Priyansh4650 <priyanshu022017@gmail.com>
2025-03-18 08:25:21 +0530

    Configure GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ShareNow/",
-  build: {
-    outDir: "dist",
-  },
   plugins: [react()],
+  base: '/ShareNow/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
