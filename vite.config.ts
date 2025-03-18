92d41b19db1ef165aa0c73f7a7468156662854d5
Priyansh4650 <priyanshu022017@gmail.com>
2025-03-18 08:16:50 +0530

    trying to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ShareNow/",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()],
 })
